fba2ccc21daa490696f77b2f48ae80caecd899cf
Tim Titeev <tim@programmer.net>
2016-03-17 17:49:28 -0400

    Initial commit

---
+++ copyrightme.py
@@ -0,0 +1,11 @@
+import sublime, sublime_plugin
+
+class CopyrightmeCommand(sublime_plugin.TextCommand):
+	def run(self, edit, text):
+		(row, col) = self.view.rowcol(0)
+		region = self.view.visible_region()
+
+		if text not in self.view.substr(region):
+			self.view.split_by_newlines(region)
+			self.view.insert(edit, 0, text + '\n\n')
+			
