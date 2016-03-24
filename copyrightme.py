1de8f8fe6c5e3d6ab225a29728ca7da688f93ba9
Tim Titeev <ttiteev@viewpost.com>
2016-03-24 18:11:56 -0400

    Removed extra files, added user preference option and minor error handling if user doesnt input text

--- copyrightme.py
+++ copyrightme.py
@@ -1,9 +1,14 @@
 import sublime, sublime_plugin
 
 class CopyrightmeCommand(sublime_plugin.TextCommand):
-	def run(self, edit, text):
+	def run(self, edit):
 		region = self.view.visible_region()
+		settings_file = sublime.load_settings('Preferences.sublime-settings');
+
+		if settings_file.has('copyright_text'):
+			text = settings_file.get('copyright_text')
+		else:
+			text = 'Please add the copyright_text to User Preferences'
 
 		if text not in self.view.substr(region):
 			self.view.insert(edit, 0, text + '\n\n')
-			
