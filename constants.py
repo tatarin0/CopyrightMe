6218f28f10916b060cf492b602bfefe0a0ce792d
tatarin0 <tim@programmer.net>
2016-03-30 18:03:08 -0400

    Added constants and made methods a little more managable.

---
+++ constants.py
@@ -0,0 +1,3 @@
+COPYRIGHT_PLACEHOLDER = 'Please add the copyright_text to User Preferences'
+SUBLIME_SETTINGS_FILE = 'Preferences.sublime-settings'
+COPYRIGHT_PRESET_OPTION = 'copyright_text'
--- copyrightme.py
+++ copyrightme.py
@@ -1,14 +1,32 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+import Copyrightme.constants
+
+# make constants a little more readable
+placeholder = Copyrightme.constants.COPYRIGHT_PLACEHOLDER
+sublime_settings = Copyrightme.constants.SUBLIME_SETTINGS_FILE
+copyright_preset = Copyrightme.constants.COPYRIGHT_PRESET_OPTION
 
 class CopyrightmeCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		region = self.view.visible_region()
-		settings_file = sublime.load_settings('Preferences.sublime-settings');
 
-		if settings_file.has('copyright_text'):
-			text = settings_file.get('copyright_text')
+	# initial
+	def run(self, document):
+		self.document = document;
+		self.region = self.view.visible_region()
+		self.settings_file = sublime.load_settings(sublime_settings)	
+
+		self.insertCopyright(self.presetCopyright());
+
+	# use preset option or if doesnt exist use placeholder
+	def presetCopyright(self):
+		if self.settings_file.has(copyright_preset):
+			text = settings_file.get(copyright_preset)
 		else:
-			text = 'Please add the copyright_text to User Preferences'
+			text = placeholder
 
-		if text not in self.view.substr(region):
-			self.view.insert(edit, 0, text + '\n\n')
+		return text;
+
+	# insert copyright text in the first line
+	def insertCopyright(self, text):
+		if text not in self.view.substr(self.region):
+			self.view.insert(self.document, 0, text + '\n\n')
